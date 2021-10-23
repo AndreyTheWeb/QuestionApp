ea47e893c28ea477ada58021a43c7961dcb9f38c
Andrey <homyachidze2014@yandex.ru>
2021-10-23 17:42:40 +0300

    final version

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import { Question } from './question';
-import { isValid } from './utils';
+import { createModal, isValid } from './utils';
+import { getAuthForm } from './auth';
+import { authWithEmailAndPassword } from './auth';
 import './style.css';
 
 const form = document.getElementById('form'),
   input = form.querySelector('#question-input'),
+  modalBtn = document.getElementById('modal-btn'),
   submitBtn = form.querySelector('#submit');
 
+window.addEventListener('load', Question.renderList);
 form.addEventListener('submit', submitFormHandler);
+modalBtn.addEventListener('click', openModal);
 input.addEventListener('input', () => {
   submitBtn.disabled = !isValid(input.value);
 });
@@ -27,5 +32,31 @@ function submitFormHandler(event) {
       input.className = '';
       submitBtn.disabled = false;
     });
+  }
+}
+
+function openModal() {
+  createModal('Авторизация', getAuthForm());
+  document.getElementById('auth-form').addEventListener('submit', authFormHandler, { once: true });
+}
+
+function authFormHandler(event) {
+  event.preventDefault();
+
+  const btn = event.target.querySelector('button');
+  const email = event.target.querySelector('#email').value;
+  const password = event.target.querySelector('#password').value;
+  btn.disabled = true;
+  authWithEmailAndPassword(email, password)
+    .then(Question.fetch)
+    .then(renderModalAfterAuth)
+    .then(() => btn.disabled = false);
+}
+
+function renderModalAfterAuth(content) {
+  if (typeof content === 'string') {
+    createModal('Ошибка!', content);
+  } else {
+    createModal('Список вопросов', Question.listToHTML(content));
   }
 }
